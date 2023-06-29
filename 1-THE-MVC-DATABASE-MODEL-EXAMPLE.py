87747ec277a3b8f32f29e99f14bac3ecfd4d60e7
uie34719 <uie34719@DEVOPS-LINUX.myguest.virtualbox.org>
2023-06-29 13:44:42 +0800

    ADD FILES

--- 1-THE-MVC-DATABASE-MODEL-EXAMPLE.py
+++ 1-THE-MVC-DATABASE-MODEL-EXAMPLE.py
@@ -1,9 +1,7 @@
-class Database:
-    def __init__(self):
-        self.accts = {'ACCT100': {'paid': 100, 'due': 200}, 'ACCT200': {'paid': 300, 'due': 350}}
+from flask import Flask
 
-    def neededdata(self, acct_input):
-        acct_output = self.accts.get(acct_input)
-        if acct_output:
-            return int(acct_output['paid']) - int(acct_output['due'])
-        return None
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
